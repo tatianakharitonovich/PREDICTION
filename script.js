24716d2729dbab7f671585208d7e220463d9af98
tatianakharitonovich <k.tanya@tut.by>
2021-06-22 11:58:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-let proxyUrl = 'https://cors-anywhere.herokuapp.com/',
-    targetUrl = 'https://api.tachyon-analytics.com/predict'
-fetch(proxyUrl + targetUrl)
-	.then((res) => res.json())
-	.then((json) => {
-		console.log(json);
-	})
-	.catch((err) => {
-		let p = document.createElement('p');
-		p.innerHTML='"Информация о пользователе не доступна"';
-		document.body.append(p);
-	});
+function showModalWin() {
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+    var modalWin = document.getElementById('popupWin');
+    modalWin.style.display = 'block';
+    let returnedUsers = document.getElementById('users');
+    let points = document.getElementById('points');
+    let buttonRetention = document.getElementById('buttonRetention');
+
+    buttonRetention.onclick= function () {
+    	let body = {
+     		returnedUsers: returnedUsers.value,
+     		points: points.value,
+    	};
+    	
+    	fetch('https://api.tachyon-analytics.com/predict/', {
+    		method: 'POST',
+    		body: JSON.stringify(body),
+    	}).then(res => res.json())
+    	.then(json => {
+    		console.log(json);
+    		let imgURL = json._url;
+    		let img = document.createElement('img');
+    		img.src = imgURL;
+    		img.width = 200;
+    		document.body.append(imgUser);
+    	})
+    	.catch((err) => {
+			let p = document.createElement('p');
+			p.innerHTML='"Информация не доступна"';
+			document.body.append(p);
+		});    	
+    	console.log(JSON.stringify(body));
+    }; 
+
+    darkLayer.onclick = function () {
+		darkLayer.parentNode.removeChild(darkLayer);
+	    modalWin.style.display = 'none';
+	    return false;
+    };
+}	
